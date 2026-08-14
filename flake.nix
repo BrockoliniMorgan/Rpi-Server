b61a106c147a1c13b097262e463a8dc1081c405a
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-08-14 21:41:42 +1000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixos-hardware,
+      nixpkgs,
+      sops-nix,
+      self,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      hostNames = builtins.concatLists (
+        builtins.genList (i: [ "qutrc-pi-${lib.fixedWidthNumber 2 i}" ]) 8
+      );
+      createSystem = hostName: {
+        ${hostName} = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          modules = [
+            nixos-hardware.nixosModules.raspberry-pi-4
+            sops-nix.nixosModules.sops
+            ./system
+          ];
+          specialArgs = {
+            inherit hostName;
+          };
+        };
+      };
+      # TODO: Clean this up to run over all self.nixosConfigurations
+      # TODO: Consider adding hostPlatform and buildPlatform to reduce needed cross-compilation
+      createSDSystem = hostName: {
+        "${hostName}-sd" =
+          ((createSystem hostName).${hostName}.extendModules {
+            modules = [
+              "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+              {
+                sdImage.compressImage = false;
+                image.fileName = "${hostName}-sdImage.img";
+              }
+            ];
+          }).config.system.build.sdImage;
+      };
+      applyToHostnames =
+        function: (builtins.foldl' (acc: new: acc // new) { } (lib.map function hostNames));
+
+    in
+    {
+      nixosConfigurations = applyToHostnames createSystem;
+      packages.x86_64-linux = applyToHostnames createSDSystem;
+    };
+}
---
+++ system/configuration.nix
@@ -0,0 +1,98 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  lib,
+  pkgs,
+  hostName,
+  ...
+}:
+{
+  networking = {
+    inherit hostName;
+  };
+  boot.supportedFilesystems.zfs = lib.mkForce false;
+
+  # Missing hdmi module (https://discourse.nixos.org/t/cannot-build-raspberry-pi-sdimage-module-dw-hdmi-not-found/71804)
+  boot.initrd.allowMissingModules = true;
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager = {
+    enable = true;
+    wifi.powersave = false;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.qutrc = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "dialout"
+      "networkmanager"
+    ];
+  };
+
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    warn-dirty = false;
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Allow for wifi forwarding to ethernet
+  networking.firewall.allowedTCPPorts = [ 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
